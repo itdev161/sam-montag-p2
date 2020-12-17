ef69a44f94a4950b7ef8b08dc8dc6ffc98340e2c
samtheman43 <montags@gmatc.matc.edu>
2020-12-16 22:06:52 -0600

    Text Addition and Colloseum shifted

--- p1-cloned/client/src/MainTextRight/TextRight.js
+++ p1-cloned/client/src/MainTextRight/TextRight.js
@@ -3,9 +3,13 @@ import React from 'react';
 const textright = () => {
     return <div>
         <h1 className="Text-header">Rome:</h1>
-        <p>This is an image of the ancient Roman Colloseum, 
-        where gladiators and beasts often fought to their death in 
-        front of crowds gathered to spectate.</p>
+        <p>
+            This is an image of the ancient Roman Colloseum, 
+            where gladiators and beasts often fought to their death in 
+            front of crowds gathered to spectate.
+        </p>
+        <h2 className="Text-header">Did You Know?</h2>
+        <p>Modern Rome has 280 fountains and more than 900 churches.</p>
     </div>
 };
 
